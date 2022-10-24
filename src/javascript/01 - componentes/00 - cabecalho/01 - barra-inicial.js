7071de76536d25af3ff40cfe243a10fd12d5213b
Matheus Serafim <matheus18serafim@gmail.com>
2022-10-24 09:39:04 -0300

    push

--- src/javascript/01 - componentes/00 - cabecalho/01 - barra-inicial.js
+++ src/javascript/01 - componentes/00 - cabecalho/01 - barra-inicial.js
@@ -1,32 +1,31 @@
 const criarConteudoBarraInicial = (textoSlogan, urlImagemSlogan) => {
+  // Adiciona texto na barra inicial
+  const slogan = document.createElement("p");
+  slogan.classList.add("slogan");
+  slogan.innerHTML = textoSlogan;
 
-    // Adiciona texto na barra inicial
-    const slogan = document.createElement("p");
-    slogan.classList.add("slogan");
-    slogan.innerHTML = textoSlogan;
+  // Adiciona imagem na barra inicial
+  const imagemSlogan = document.createElement("img");
+  imagemSlogan.classList.add("imagemSlogan");
+  imagemSlogan.src = urlImagemSlogan;
+  imagemSlogan.alt = "imagem-slogan";
 
-    // Adiciona imagem na barra inicial
-    const imagemSlogan = document.createElement("img");
-    imagemSlogan.classList.add("imagemSlogan");
-    imagemSlogan.src = urlImagemSlogan;
-    imagemSlogan.alt = "imagem-slogan";
+  // Seleciona barra inicial e adiciona elementos
+  let barraInicialContainer = document.createElement("div");
+  barraInicialContainer.classList.add("barra-inicial-container");
+  barraInicialContainer.append(slogan);
+  // barraInicialContainer.append(imagemSlogan);
 
-    // Seleciona barra inicial e adiciona elementos
-    let barraInicialContainer = document.createElement("div");
-    barraInicialContainer.classList.add("barra-inicial-container");
-    barraInicialContainer.append(slogan);
-    // barraInicialContainer.append(imagemSlogan);
-
-    return barraInicialContainer;
-}
+  return barraInicialContainer;
+};
 
 // BACKUP DE LOGICA DE IMPLEMENTAÇÃO, INSERIR NAS PÁGINAS
-window.addEventListener('load', () => {
-    'strict'
-    try {
-        removerTudo(".barra-inicial .conteiner");
-        removerTudo(".atalhos-mobile");
-    } catch (error) {
-        console.log(`Erro encontrado -> ${error}`);
-    }
+window.addEventListener("load", () => {
+  "strict";
+  try {
+    removerTudo(".barra-inicial .conteiner");
+    removerTudo(".atalhos-mobile");
+  } catch (error) {
+    console.log(`Erro encontrado -> ${error}`);
+  }
 });
