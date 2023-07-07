a8a510bae4b69d33dab20b09d1ad77b64151e2ee
mBalao9 <jessi.hane@gmail.com>
2023-07-07 17:04:33 -0300

    whats

--- src/javascript/01 - componentes/10 - Whats/whats.js
+++ src/javascript/01 - componentes/10 - Whats/whats.js
@@ -3,35 +3,35 @@ window.addEventListener("load", () => {
   const whats = document.createElement("a");
   whats.innerHTML = `<img src='https://cdn.awsli.com.br/2289/2289041/arquivos/Icon.svg' alt ="whats" />`;
 
-  const boxWhats = document.createElement("div");
-  boxWhats.classList.add("box-whats");
+  // const boxWhats = document.createElement("div");
+  // boxWhats.classList.add("box-whats");
 
-  boxWhats.innerHTML = `<div class="content-whats">
-      <ul>
-          <li>
-              <a href="https://api.whatsapp.com/send?phone=551147570111" target="_blank">
-                SAC
-              </a>
-          </li>
-          <li>
-              <a href="https://api.whatsapp.com/send?phone=5511917899204" target="_blank">
-                Comercial
-              </a>
-          </li>
-          </ul>
-      </div>`;
+  // boxWhats.innerHTML = `<div class="content-whats">
+  //     <ul>
+  //         <li>
+  //             <a href="https://api.whatsapp.com/send?phone=551147570111" target="_blank">
+  //               SAC
+  //             </a>
+  //         </li>
+  //         <li>
+  //             <a href="https://api.whatsapp.com/send?phone=5511917899204" target="_blank">
+  //               Comercial
+  //             </a>
+  //         </li>
+  //         </ul>
+  //     </div>`;
 
-  boxWhats.style.display = "none";
+  // boxWhats.style.display = "none";
 
-  whats.addEventListener("click", function () {
-    if (boxWhats.style.display === "none") {
-      boxWhats.style.display = "block";
-    } else {
-      boxWhats.style.display = "none";
-    }
-  });
+  // whats.addEventListener("click", function () {
+  //   if (boxWhats.style.display === "none") {
+  //     boxWhats.style.display = "block";
+  //   } else {
+  //     boxWhats.style.display = "none";
+  //   }
+  // });
 
-  // whats.href = "https://api.whatsapp.com/send?phone=5511916639586";
+  whats.href = "https://api.whatsapp.com/send?phone=5511917899204";
   whats.id = "whats";
   whats.target = "_blank";
   footerZ.append(whats);
